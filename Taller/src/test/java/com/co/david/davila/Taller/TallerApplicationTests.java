8d3df0a59c5d9cece6b1b3102220b5eb79ddde5e
DavidZ28 <santi20052012@hotmail.com>
2024-08-30 18:58:23 -0500

    Primer commit - Subiendo mi proyecto

---
+++ Taller/src/test/java/com/co/david/davila/Taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.david.davila.Taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
